f26b102ce7f12556bc8d248787ebac623fbb1909
Alex Roman <eirexe123@gmail.com>
2018-08-28 18:49:07 +0200

    Permitir que la descripción de las categorias del foro puedan estar vacias

--- figurabackend/FigureSite/models.py
+++ figurabackend/FigureSite/models.py
@@ -23,7 +23,7 @@ class User(AbstractUser):
 
 class ForumCategory(OrderedModel):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.name
