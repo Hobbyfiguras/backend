c1fe2a6cc63b2b2379e93387ea049c9102ccf3a4
Alex Roman <eirexe123@gmail.com>
2018-08-31 19:09:22 +0200

    Permitir que los usurios se logeen con su usuario en mayuscula o minuscula

--- figurabackend/FigureSite/models.py
+++ figurabackend/FigureSite/models.py
@@ -1,12 +1,16 @@
 import os
 import random
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django_resized import ResizedImageField
 from django.db import models
 from django.utils.deconstruct import deconstructible
 from ordered_model.models import OrderedModel
 from django.utils.text import slugify
+
+class MyUserManager(BaseUserManager):
+    def get_by_natural_key(self, username):
+        return self.get(username__iexact=username)
 
 @deconstructible
 class AvatarRename(object):
@@ -21,8 +25,10 @@ class AvatarRename(object):
 avatar_rename = AvatarRename()
 
 class User(AbstractUser):
+    objects = MyUserManager()
     avatar = ResizedImageField(size=[256, 256], crop=['middle', 'center'], force_format='JPEG', upload_to=avatar_rename, null=True, blank=True)
-
+    def get_by_natural_key(self, username):
+        return self.get(**{self.model.USERNAME_FIELD + '__iexact': username})
 class ForumCategory(OrderedModel):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200, null=True, blank=True)
