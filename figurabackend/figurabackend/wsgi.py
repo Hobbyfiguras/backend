409d81312ad22332648a0ad51cf2f9bee5f635f6
Alex Roman <eirexe123@gmail.com>
2018-09-12 03:51:47 +0200

    add missing requiement

--- figurabackend/figurabackend/wsgi.py
+++ figurabackend/figurabackend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'figurabackend.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'figurabackend.settings_prod')
 
 application = get_wsgi_application()
