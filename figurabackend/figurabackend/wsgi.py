dc6e80129d6e0501dcbe0bd809b7058d7ea34fb8
Alex Roman <eirexe123@gmail.com>
2018-09-12 03:37:22 +0200

    make uwsgi use prod

--- figurabackend/figurabackend/wsgi.py
+++ figurabackend/figurabackend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'figurabackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'figurabackend.prod')
 
 application = get_wsgi_application()
