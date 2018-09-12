35f15fd2b731a893d0f12f4336152018b513f382
Alex Roman <eirexe123@gmail.com>
2018-09-12 04:53:01 +0200

    Update UWSGI settings

--- figurabackend/figurabackend/settings_prod.py
+++ figurabackend/figurabackend/settings_prod.py
@@ -23,7 +23,7 @@ with open('/etc/secret_key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '95.179.177.28']
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
