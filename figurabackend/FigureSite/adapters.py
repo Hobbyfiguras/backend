9b07f1ce511fd788949cba0198982d4645d9e79a
Alex Roman <eirexe123@gmail.com>
2018-10-07 21:44:12 +0200

    Quitar sujeto

--- figurabackend/FigureSite/adapters.py
+++ figurabackend/FigureSite/adapters.py
@@ -1,5 +1,7 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.utils import build_absolute_uri
+from django.utils.encoding import force_text
+
 class FigureSiteAccountAdapter(DefaultAccountAdapter):
     def get_email_confirmation_url(self, request, emailconfirmation):
         """Constructs the email confirmation (activation) url.
@@ -12,3 +14,6 @@ class FigureSiteAccountAdapter(DefaultAccountAdapter):
             request,
             url)
         return ret
+    
+    def format_email_subject(self, subject):
+        return force_text(subject)
