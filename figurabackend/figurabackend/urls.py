b99b8d5928ee5060054c0d8888454cc1ac152bd6
Alex Roman <eirexe123@gmail.com>
2018-11-04 23:17:20 +0100

    disable CSRF in some cases

--- figurabackend/figurabackend/urls.py
+++ figurabackend/figurabackend/urls.py
@@ -20,6 +20,7 @@ from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from FigureSite.auth import CustomPasswordResetView
 from rest_auth.registration.views import RegisterView, VerifyEmailView
 from rest_auth.views import (
@@ -27,18 +28,24 @@ from rest_auth.views import (
     PasswordResetView, PasswordResetConfirmView
 )
 
+from django.middleware.csrf import get_token
+
 from rest_framework_simplejwt.views import (
     TokenRefreshView
 )
 
 from FigureSite.auth import TokenObtainPairView
 
+@method_decorator(csrf_exempt, name='dispatch')
+class VerifyEmailViewNoCSRF(VerifyEmailView):
+    pass
+
 urlpatterns = [
     path('djadmin/', admin.site.urls),
     path('api/mfc/', include('mfc.urls')),
     path('api/', include('FigureSite.urls')),
     path('api/auth/register/', RegisterView.as_view()),
-    path('api/auth/register/verify-email/', VerifyEmailView.as_view()),
+    path('api/auth/register/verify-email/', VerifyEmailViewNoCSRF.as_view()),
     path('api/auth/change_password/', csrf_exempt(PasswordChangeView.as_view())),
     path('api/auth/password_reset/', CustomPasswordResetView.as_view(), name='password_reset_confirm'),
     path('api/auth/password_reset/verify/', csrf_exempt(PasswordResetConfirmView.as_view())),
