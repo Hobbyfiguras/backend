d118782926b2353dd66fe638a17a71c31694b3ae
Alex Roman <eirexe123@gmail.com>
2018-11-16 00:33:07 +0100

    Add myself to default admins

--- figurabackend/figurabackend/settings_prod.py
+++ figurabackend/figurabackend/settings_prod.py
@@ -63,28 +63,4 @@ EMAIL_HOST_USER = 'noreply@hobbyfiguras.moe'
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = 'Hobbyfiguras <noreply@hobbyfiguras.moe>'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
-        }
-    },
-    'handlers': {
-        'gunicorn': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'formatter': 'verbose',
-            'filename': '/server/gunicorn.errors',
-            'maxBytes': 1024 * 1024 * 100,  # 100 mb
-        }
-    },
-    'loggers': {
-        'gunicorn.errors': {
-            'level': 'DEBUG',
-            'handlers': ['gunicorn'],
-            'propagate': True,
-        },
-    }
-}
+ADMINS = [('Alex Roman', 'eirexe@hobbyfiguras.moe')]
