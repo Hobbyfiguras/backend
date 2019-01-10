d6f1bd8b61af7bc5c506d0106e5da056b3fd8a6b
Alex Roman <eirexe123@gmail.com>
2019-01-10 02:23:16 +0100

    Add psycopg2cffi support

--- figurabackend/figurabackend/shared_settings.py
+++ figurabackend/figurabackend/shared_settings.py
@@ -4,6 +4,13 @@ import corsheaders # for pireqs
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+try:
+    import psycopg2
+except ImportError:
+    # Fall back to psycopg2cffi
+    from psycopg2cffi import compat
+    compat.register()
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
