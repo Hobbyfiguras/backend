360b639e5169399493c799db921300b95dc4b452
Álex Román Núñez <eirexe123@gmail.com>
2020-05-07 21:18:54 +0200

    highlight

--- figurabackend/FigureSite/models.py
+++ figurabackend/FigureSite/models.py
@@ -17,9 +17,12 @@ from django.utils import timezone
 from mfc import mfc_api
 from django.http import Http404
 from guardian.mixins import GuardianUserMixin
+
+
 class MyUserManager(UserManager):
     def get_by_natural_key(self, username):
         return self.get(username__iexact=username)
+
 
 @deconstructible
 class AvatarRename(object):
@@ -27,10 +30,12 @@ class AvatarRename(object):
         # Remove the extension from the filename
         ext = filename.split('.')[-1]
 
-        filename = '{}.{}'.format(external_id_from_model_and_internal_id(apps.get_model(app_label="FigureSite", model_name="User"), instance.id), ext)
+        filename = '{}.{}'.format(external_id_from_model_and_internal_id(
+            apps.get_model(app_label="FigureSite", model_name="User"), instance.id), ext)
         print("avatar: %s" % filename)
         # return the whole path to the file
         return os.path.join("avatars", filename)
+
 
 @deconstructible
 class ForumIconRename(object):
@@ -42,6 +47,7 @@ class ForumIconRename(object):
         # return the whole path to the file
         return os.path.join("forum_icons", filename)
 
+
 @deconstructible
 class VoteTypeRename(object):
     def __call__(self, instance, filename):
@@ -52,13 +58,16 @@ class VoteTypeRename(object):
         # return the whole path to the file
         return os.path.join("forum_votes", filename)
 
+
 avatar_rename = AvatarRename()
 forum_icon_rename = ForumIconRename()
 vote_type_rename = VoteTypeRename()
 
+
 class User(AbstractUser, GuardianUserMixin):
     objects = MyUserManager()
-    avatar = ResizedImageField(size=[256, 256], crop=['middle', 'center'], force_format='JPEG', upload_to=avatar_rename, null=True, blank=True)
+    avatar = ResizedImageField(size=[256, 256], crop=[
+                               'middle', 'center'], force_format='JPEG', upload_to=avatar_rename, null=True, blank=True)
     mal_username = models.CharField(max_length=80, null=True, blank=True)
     anilist_username = models.CharField(max_length=80, null=True, blank=True)
     mfc_username = models.CharField(max_length=80, null=True, blank=True)
@@ -104,7 +113,7 @@ class User(AbstractUser, GuardianUserMixin):
     @authenticated_users
     def has_write_permission(request):
         return True
-        
+
     @allow_staff_or_superuser
     def has_object_write_permission(self, request):
         print(self.id)
@@ -126,6 +135,7 @@ class User(AbstractUser, GuardianUserMixin):
         print(request.user.id)
         if self == request.user:
             return True
+
 
 class ForumCategory(OrderedModel):
     name = models.CharField(max_length=100)
@@ -150,21 +160,26 @@ class ForumCategory(OrderedModel):
 
     def __str__(self):
         return self.name
+
     class Meta:
         verbose_name_plural = "forum categories"
+
     def save(self, *args, **kwargs):
         # Only save slugs on first save
         if not self.id:
             unique_slugify(self, self.name)
-        super(ForumCategory, self).save(*args,**kwargs)
+        super(ForumCategory, self).save(*args, **kwargs)
+
 
 class Forum(OrderedModel):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
-    category = models.ForeignKey(ForumCategory, related_name="forums", on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        ForumCategory, related_name="forums", on_delete=models.CASCADE)
     slug = models.SlugField(max_length=100, blank=True, unique=True)
     only_staff_can_post = models.BooleanField(default=False)
-    icon = ResizedImageField(size=[128, 128], crop=['middle', 'center'], upload_to=forum_icon_rename, force_format='PNG', null=True, blank=True)
+    icon = ResizedImageField(size=[128, 128], crop=[
+                             'middle', 'center'], upload_to=forum_icon_rename, force_format='PNG', null=True, blank=True)
     order_with_respect_to = 'category'
 
     class Meta:
@@ -207,15 +222,19 @@ class Forum(OrderedModel):
 
     def __str__(self):
         return self.name
+
     def save(self, *args, **kwargs):
         # Only save slugs on first save
         if not self.id:
             unique_slugify(self, self.name)
-        super(Forum, self).save(*args,**kwargs)    
+        super(Forum, self).save(*args, **kwargs)
+
 
 class PrivateMessage(models.Model):
-    creator = models.ForeignKey(User, related_name="messages_sent", on_delete=models.CASCADE)
-    receiver = models.ForeignKey(User, related_name="messages_received", on_delete=models.CASCADE)
+    creator = models.ForeignKey(
+        User, related_name="messages_sent", on_delete=models.CASCADE)
+    receiver = models.ForeignKey(
+        User, related_name="messages_received", on_delete=models.CASCADE)
     subject = models.TextField(max_length=500)
     content = models.TextField(max_length=20000)
     created = models.DateTimeField(editable=False)
@@ -251,9 +270,11 @@ class PrivateMessage(models.Model):
             self.created = timezone.now()
         return super(PrivateMessage, self).save(*args, **kwargs)
 
+
 class MFCItem(models.Model):
     name = models.TextField()
     created = models.DateTimeField(editable=False)
+
     def save(self, *args, **kwargs):
         ''' On save, update timestamps '''
         if not self.id:
@@ -269,22 +290,30 @@ class MFCItem(models.Model):
                 mfc_item = mfc_api.get_figure_data(id)
             except:
                 return None
-            # HACK: Django doesn't really call our overriden save method from inside a classmethod, so we have to add created here 
-            item = cls(id=mfc_item['id'], name=mfc_item['name'], created=timezone.now())
+            # HACK: Django doesn't really call our overriden save method from inside a classmethod, so we have to add created here
+            item = cls(id=mfc_item['id'],
+                       name=mfc_item['name'], created=timezone.now())
             item.save()
         return item
+
+
 class Thread(models.Model):
     title = models.CharField(max_length=300)
-    creator = models.ForeignKey(User, related_name="threads", on_delete=models.CASCADE)
-    forum = models.ForeignKey(Forum, related_name="threads", on_delete=models.CASCADE)
+    creator = models.ForeignKey(
+        User, related_name="threads", on_delete=models.CASCADE)
+    forum = models.ForeignKey(
+        Forum, related_name="threads", on_delete=models.CASCADE)
     created = models.DateTimeField(editable=False)
     modified = models.DateTimeField(editable=False)
     slug = models.SlugField(max_length=100, blank=True, unique=True)
     nsfw = models.BooleanField(default=False)
     is_sticky = models.BooleanField(default=False)
     is_closed = models.BooleanField(default=False)
-    subscribers = models.ManyToManyField(User, related_name="subscribed_threads")
-    related_items = models.ManyToManyField(MFCItem, related_name="related_threads")
+    subscribers = models.ManyToManyField(
+        User, related_name="subscribed_threads")
+    related_items = models.ManyToManyField(
+        MFCItem, related_name="related_threads")
+    highlighted = models.BooleanField(default=False)
 
     @property
     def hid(self):
@@ -320,6 +349,7 @@ class Thread(models.Model):
     @authenticated_users
     def has_make_nsfw_permission(request):
         return True
+
     @allow_staff_or_superuser
     def has_object_make_nsfw_permission(self, request):
         if self.creator == request.user:
@@ -331,6 +361,7 @@ class Thread(models.Model):
     @authenticated_users
     def has_move_thread_permission(request):
         return True
+
     @allow_staff_or_superuser
     def has_object_move_thread_permission(self, request):
         return False
@@ -341,6 +372,7 @@ class Thread(models.Model):
             self.modified = timezone.now()
         unique_slugify(self, self.title)
         return super(Thread, self).save(*args, **kwargs)
+
     @property
     def last_post(self):
         ordered_posts = self.posts.all().order_by('-created')
@@ -360,25 +392,27 @@ class Thread(models.Model):
     @staticmethod
     def has_read_permission(request):
         return True
-    
+
     def has_object_read_permission(self, request):
         return True
-    
-    @staticmethod    
+
+    @staticmethod
     @authenticated_users
     def has_create_post_permission(request):
         if not request.user.is_banned:
             return True
         else:
             return False
+
     @authenticated_users
     def has_object_create_post_permission(self, request):
         return True
 
-    @staticmethod    
+    @staticmethod
     @authenticated_users
     def has_change_subscription_permission(request):
         return True
+
     @authenticated_users
     def has_object_change_subscription_permission(self, request):
         return True
@@ -386,18 +420,24 @@ class Thread(models.Model):
     def __str__(self):
         return self.title
 
+
 class Post(models.Model):
-    creator = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
-    thread = models.ForeignKey(Thread, related_name="posts", on_delete=models.CASCADE)
+    creator = models.ForeignKey(
+        User, related_name="posts", on_delete=models.CASCADE)
+    thread = models.ForeignKey(
+        Thread, related_name="posts", on_delete=models.CASCADE)
     content = models.TextField(max_length=40000)
     created = models.DateTimeField(editable=False)
     modified = models.DateTimeField(editable=False)
     deleted = models.BooleanField(default=False)
     delete_reason = models.TextField(default='')
-    modified_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    modified_by = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.CASCADE)
+
     @property
     def hid(self):
         return external_id_from_model_and_internal_id(Post, self.id)
+
     @property
     def page(self):
         return int(self.__class__.objects.filter(thread=self.thread).filter(
@@ -459,6 +499,7 @@ class Post(models.Model):
         except IntegrityError:
             return 'already_reported'
         return 'ok'
+
     def save(self, *args, **kwargs):
         ''' On save, update timestamps '''
         if not self.id:
@@ -466,10 +507,12 @@ class Post(models.Model):
             self.modified = timezone.now()
         return super(Post, self).save(*args, **kwargs)
 
-    
+
 class Report(models.Model):
-    creator = models.ForeignKey(User, related_name="reports", on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, related_name="reports", on_delete=models.CASCADE)
+    creator = models.ForeignKey(
+        User, related_name="reports", on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name="reports", on_delete=models.CASCADE)
     reason = models.TextField()
     created = models.DateTimeField(editable=False)
 
@@ -496,35 +539,46 @@ class Report(models.Model):
         if not self.id:
             self.created = timezone.now()
         return super(Report, self).save(*args, **kwargs)
+
     class Meta:
         unique_together = ('creator', 'post')
 
+
 class VoteType(OrderedModel):
     name = models.CharField(max_length=80)
-    icon = ResizedImageField(size=[32, 32], crop=['middle', 'center'], force_format='PNG', upload_to=vote_type_rename)
+    icon = ResizedImageField(size=[32, 32], crop=[
+                             'middle', 'center'], force_format='PNG', upload_to=vote_type_rename)
     slug = models.SlugField(max_length=100, blank=True, unique=True)
+
     def __str__(self):
         return self.name
+
     def save(self, *args, **kwargs):
         # Only save slugs on first save
         if not self.id:
             unique_slugify(self, self.name)
-        super(VoteType, self).save(*args,**kwargs)
+        super(VoteType, self).save(*args, **kwargs)
+
 
 class UserVote(models.Model):
-    user = models.ForeignKey(User, related_name="votes", on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, related_name="votes", on_delete=models.CASCADE)
-    vote_type = models.ForeignKey(VoteType, related_name="votes", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="votes",
+                             on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name="votes",
+                             on_delete=models.CASCADE)
+    vote_type = models.ForeignKey(
+        VoteType, related_name="votes", on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('user', 'post')
+
 
 class Notification(models.Model):
     # notification_post_sub
     notification_type = models.CharField(max_length=200)
     created = models.DateTimeField(editable=False)
     actor = models.ForeignKey(User, related_name='+', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name='notifications', on_delete=models.CASCADE)
     object_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     notification_object = GenericForeignKey('object_type', 'object_id')
@@ -554,13 +608,17 @@ class Notification(models.Model):
             self.created = timezone.now()
         return super(Notification, self).save(*args, **kwargs)
 
+
 class BanReason(models.Model):
     # notification_post_sub
     created = models.DateTimeField(editable=False)
-    post = models.ForeignKey(Post, related_name="bans", on_delete=models.CASCADE, null=True, blank=True)
+    post = models.ForeignKey(Post, related_name="bans",
+                             on_delete=models.CASCADE, null=True, blank=True)
     ban_reason = models.TextField(max_length=1000)
-    banned_user = models.ForeignKey(User, related_name="bans", on_delete=models.CASCADE)
-    banner = models.ForeignKey(User, related_name="+", on_delete=models.CASCADE)
+    banned_user = models.ForeignKey(
+        User, related_name="bans", on_delete=models.CASCADE)
+    banner = models.ForeignKey(
+        User, related_name="+", on_delete=models.CASCADE)
     ban_expiry_date = models.DateTimeField(editable=False)
 
     def save(self, *args, **kwargs):
@@ -568,4 +626,3 @@ class BanReason(models.Model):
         if not self.id:
             self.created = timezone.now()
         return super(BanReason, self).save(*args, **kwargs)
-
--- figurabackend/FigureSite/views/thread_views.py
+++ figurabackend/FigureSite/views/thread_views.py
@@ -14,15 +14,19 @@ from drf_haystack.viewsets import HaystackViewSet
 from dry_rest_permissions.generics import DRYPermissions
 
 from django.utils import timezone
+from django.db.models import Q
 
 from FigureSite.models import Thread, Notification, Forum, MFCItem
 from FigureSite import serializers
 from FigureSite.search_indexes import ThreadIndex
 from FigureSite.notifications import send_notification
+
+
 class ThreadPagination(PageNumberPagination):
     page_size = 20
     max_page_size = 20
     page_size_query_param = 'page_size'
+
 
 class ThreadSearchSerializer(HaystackSerializer):
     class Meta:
@@ -33,7 +37,9 @@ class ThreadSearchSerializer(HaystackSerializer):
         # The `fields` contains all the fields we want to include.
         # NOTE: Make sure you don't confuse these with model attributes. These
         # fields belong to the search index!
-        fields = [ "text", "username", "slug", "nsfw", "thread_id", "forum", "modified", "created", "post_count", "last_post_creator" ]
+        fields = ["text", "username", "slug", "nsfw", "thread_id", "forum",
+                  "modified", "created", "post_count", "last_post_creator"]
+
 
 class ThreadSearchView(HaystackViewSet):
     index_models = [Thread]
@@ -44,117 +50,138 @@ class ThreadSearchView(HaystackViewSet):
     pagination_class = ThreadPagination
     serializer_class = ThreadSearchSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     def filter_queryset(self, queryset):
-      queryset = super(ThreadSearchView, self).filter_queryset(queryset)
-      return queryset.order_by('-created')
+        queryset = super(ThreadSearchView, self).filter_queryset(queryset)
+        return queryset.order_by('-created')
+
 
 class ThreadViewSet(ExternalIdViewMixin, mixins.UpdateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
-  queryset = Thread.objects.all()
-  permission_classes = (DRYPermissions,)
-  serializer_class = serializers.ThreadSerializer
-  pagination_class = ThreadPagination
+    queryset = Thread.objects.all()
+    permission_classes = (DRYPermissions,)
+    serializer_class = serializers.ThreadSerializer
+    pagination_class = ThreadPagination
 
-  def filter_queryset(self, queryset):
-    queryset = super(ThreadViewSet, self).filter_queryset(queryset)
-    return queryset.order_by('-modified')
+    def filter_queryset(self, queryset):
+        queryset = super(ThreadViewSet, self).filter_queryset(queryset)
+        return queryset.order_by('-modified')
 
-  def get_serializer_class(self):
-    if self.action == 'create' or 'update' or 'partial_update':
-      if self.request.user.is_staff:
-        return serializers.FullCreateThreadSerializer 
-      else:
-        return serializers.ThreadSerializer
-    else:
-      return serializers.FullThreadSerializer
-  def retrieve(self, request, pk=None):
-    thread = self.get_object()
-    page = self.paginate_queryset(thread.posts.all().order_by('created'))
-    posts = {}
-    if page is not None:
-      serializer = serializers.PostSerializer(page, many=True, context={'request': request})
-      posts = self.get_paginated_response(serializer.data).data
-    else:
-      posts = serializers.PostSerializer(thread.posts, many=True, context={'request': request})
-    return Response({**{'posts': posts, 'subscribed': request.user in thread.subscribers.all()}, **serializers.FullThreadSerializer(thread, context={'request': request}).data})
+    def get_serializer_class(self):
+        if self.action == 'create' or 'update' or 'partial_update':
+            if self.request.user.is_staff:
+                return serializers.FullCreateThreadSerializer
+            else:
+                return serializers.ThreadSerializer
+        else:
+            return serializers.FullThreadSerializer
 
-  @action(detail=True, methods=['post'])
-  def change_subscription(self, request, pk=None):
-    thread = self.get_object()
-    if 'subscribed' in request.data:
-      thread.change_user_subscription(request.user, request.data['subscribed'])
-      return Response({}, status=status.HTTP_200_OK)
-    else:
-      return Response({}, status=status.HTTP_400_BAD_REQUEST)
+    def retrieve(self, request, pk=None):
+        thread = self.get_object()
+        page = self.paginate_queryset(thread.posts.all().order_by('created'))
+        posts = {}
+        if page is not None:
+            serializer = serializers.PostSerializer(
+                page, many=True, context={'request': request})
+            posts = self.get_paginated_response(serializer.data).data
+        else:
+            posts = serializers.PostSerializer(
+                thread.posts, many=True, context={'request': request})
+        return Response({**{'posts': posts, 'subscribed': request.user in thread.subscribers.all()}, **serializers.FullThreadSerializer(thread, context={'request': request}).data})
 
-  @action(detail=True, methods=['post'])
-  def move_thread(self, request, pk=None):
-    thread = self.get_object()
-    print(request.data)
-    if 'forum' in request.data:
-      forum = Forum.objects.get(id=internal_id_from_model_and_external_id(Forum, request.data['forum']))
-      if forum:
-        thread.forum = forum
+    @action(detail=False, methods=['get'])
+    def front_page(self, request, pk=None):
+        page = self.paginate_queryset(
+            Thread.objects.filter(Q(forum__slug='noticias-y-novedades') | Q(highlighted=True)).order_by('-created'))
+        print(page)
+        if page is not None:
+            serializer = serializers.ThreadSerializer(
+                page, many=True, context={'request': request})
+            return self.get_paginated_response(serializer.data)
+        return Response({}, status=status.HTTP_404_NOT_FOUND)
+
+    @action(detail=True, methods=['post'])
+    def change_subscription(self, request, pk=None):
+        thread = self.get_object()
+        if 'subscribed' in request.data:
+            thread.change_user_subscription(
+                request.user, request.data['subscribed'])
+            return Response({}, status=status.HTTP_200_OK)
+        else:
+            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'])
+    def move_thread(self, request, pk=None):
+        thread = self.get_object()
+        print(request.data)
+        if 'forum' in request.data:
+            forum = Forum.objects.get(
+                id=internal_id_from_model_and_external_id(Forum, request.data['forum']))
+            if forum:
+                thread.forum = forum
+                thread.save()
+                return Response({}, status=status.HTTP_200_OK)
+            else:
+                return Response({}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'])
+    def make_nsfw(self, request, pk=None):
+        thread = self.get_object()
+        thread.nsfw = True
         thread.save()
         return Response({}, status=status.HTTP_200_OK)
-      else:
-        return Response({}, status=status.HTTP_404_NOT_FOUND)
-    else:
-      return Response({}, status=status.HTTP_400_BAD_REQUEST)
-  @action(detail=True, methods=['post'])
-  def make_nsfw(self, request, pk=None):
-    thread = self.get_object()
-    thread.nsfw = True
-    thread.save()
-    return Response({}, status=status.HTTP_200_OK)
-  @action(detail=True, methods=['post'])
-  def create_post(self, request, pk=None):
-    thread = self.get_object()
-    request.data['creator'] = request.user.id
-    request.data['thread'] = thread.id
-    serializer = serializers.CreatePostSerializer(data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      thread.modified = timezone.now()
-      thread.save()
 
-      for subscriber in thread.subscribers.all():
-        # Users shouldn't get notifications about themselves
-        if request.user.id != subscriber.id:
-          notification = Notification.objects.create(notification_type="notification_post_sub", user = subscriber, actor=request.user, notification_object=serializer.instance)
-          notification.save()
-          send_notification(request, notification)
+    @action(detail=True, methods=['post'])
+    def create_post(self, request, pk=None):
+        thread = self.get_object()
+        request.data['creator'] = request.user.id
+        request.data['thread'] = thread.id
+        serializer = serializers.CreatePostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            thread.modified = timezone.now()
+            thread.save()
 
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    else:
-      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            for subscriber in thread.subscribers.all():
+                # Users shouldn't get notifications about themselves
+                if request.user.id != subscriber.id:
+                    notification = Notification.objects.create(
+                        notification_type="notification_post_sub", user=subscriber, actor=request.user, notification_object=serializer.instance)
+                    notification.save()
+                    send_notification(request, notification)
 
-  def get_throttles(self):
-      if self.action == 'create_post':
-          self.throttle_scope = 'posts'
-      else:
-          self.throttle_scope = None
-      return super().get_throttles()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-  def update(self, request, *args, **kwargs):
-    related_items = []
-    if 'related_items' in request.data:
-      for item in request.data['related_items']:
-        db_item = MFCItem.get_or_fetch_mfc_item(item)
-        if db_item:
-          related_items.append(db_item)
-      del request.data['related_items']
-    partial = kwargs.pop('partial', False)
-    instance = self.get_object()
-    serializer = self.get_serializer(instance, data=request.data, partial=partial)
-    serializer.is_valid(raise_exception=True)
+    def get_throttles(self):
+        if self.action == 'create_post':
+            self.throttle_scope = 'posts'
+        else:
+            self.throttle_scope = None
+        return super().get_throttles()
 
-    thread = serializer.save()
-    thread.related_items.set(related_items)
-    thread.save()
-    if getattr(instance, '_prefetched_objects_cache', None):
-      # If 'prefetch_related' has been applied to a queryset, we need to
-      # forcibly invalidate the prefetch cache on the instance.
-      instance._prefetched_objects_cache = {}
+    def update(self, request, *args, **kwargs):
+        related_items = []
+        if 'related_items' in request.data:
+            for item in request.data['related_items']:
+                db_item = MFCItem.get_or_fetch_mfc_item(item)
+                if db_item:
+                    related_items.append(db_item)
+            del request.data['related_items']
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
 
+        thread = serializer.save()
+        thread.related_items.set(related_items)
+        thread.save()
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
 
-    return Response(serializer.data)
+        return Response(serializer.data)
